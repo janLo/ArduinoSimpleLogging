b983abe60a22b79cb4de3a62dae9346d3457afb2
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-12-12 02:00:28 +0100

    Add implementation for the logging library.
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

---
+++ src/ArduinoSimpleLogging.cpp
@@ -0,0 +1,65 @@
+/**
+ * ArduinoSimpleLogging
+ *
+ * https://github.com/janLo/ArduinoSimpleLogging
+ *
+ * Copyright (c) 2017 Jan Losinski.
+ * MIT License
+ */
+
+#include "ArduinoSimpleLogging.h"
+
+size_t ArduinoSimpleLogging::LogTarget::write(uint8_t byte) {
+  return Logger.log(_level, byte);
+}
+
+size_t ArduinoSimpleLogging::LogTarget::write(const uint8_t *buffer, size_t size) {
+  return Logger.log(_level, buffer, size);
+}
+
+size_t ArduinoSimpleLogging::log(ArduinoSimpleLogging::Level level, const uint8_t *buffer,
+                           size_t size) {
+  for (auto &handler : handlers) {
+    if ((handler.mask & level) != 0) {
+      handler.stream.write(buffer, size);
+    }
+  }
+  return size;
+}
+
+size_t ArduinoSimpleLogging::log(ArduinoSimpleLogging::Level level, uint8_t byte) {
+  for (auto &handler : handlers) {
+    if ((handler.mask & level) != 0) {
+      handler.stream.write(byte);
+    }
+  }
+
+  return 1;
+}
+
+uint8_t ArduinoSimpleLogging::LogHandler::makeMask(
+    const ArduinoSimpleLogging::Level level) {
+  uint8_t mask = 0x0;
+  while (mask < level) {
+    mask = (mask << 1) | (uint8_t)0x01;
+  }
+  return mask;
+}
+
+void ArduinoSimpleLogging::addHandler(ArduinoSimpleLogging::Level level, Print &stream) {
+  removeHandler(stream);
+  handlers.emplace_front(level, stream);
+}
+
+void ArduinoSimpleLogging::removeHandler(Print &stream) {
+  handlers.remove_if([&stream](const LogHandler &handler) {
+    return &(handler.stream) == &stream;
+  });
+}
+
+ArduinoSimpleLogging Logger;
+
+ArduinoSimpleLogging::LogTarget ArduinoSimpleLogging::debug(DEBUG);
+ArduinoSimpleLogging::LogTarget ArduinoSimpleLogging::info(INFO);
+ArduinoSimpleLogging::LogTarget ArduinoSimpleLogging::warning(WARNING);
+ArduinoSimpleLogging::LogTarget ArduinoSimpleLogging::error(ERROR);
---
+++ src/ArduinoSimpleLogging.h
@@ -0,0 +1,65 @@
+/**
+ * ArduinoSimpleLogging
+ *
+ * https://github.com/janLo/ArduinoSimpleLogging
+ *
+ * Copyright (c) 2017 Jan Losinski.
+ * MIT License
+ */
+
+#ifndef ARDUINOLOGGING_H
+#define ARDUINOLOGGING_H
+
+#include <forward_list>
+
+#include <Print.h>
+
+class ArduinoSimpleLogging {
+ public:
+  enum Level {
+    ERROR = 0x01,
+    WARNING = (0x01 << 1),
+    INFO = (0x01 << 2),
+    DEBUG = (0x01 << 3)
+  };
+
+  class LogTarget : public Print {
+   public:
+    explicit LogTarget(const Level level) : _level(level) {}
+    LogTarget(const LogTarget &) = delete;
+    LogTarget(const LogTarget &&) = delete;
+
+    size_t write(uint8_t) override;
+    size_t write(const uint8_t *buffer, size_t size) override;
+
+   private:
+    Level _level;
+  };
+
+  static LogTarget debug;
+  static LogTarget info;
+  static LogTarget warning;
+  static LogTarget error;
+
+  size_t log(Level level, const uint8_t *buffer, size_t size);
+  size_t log(Level level, uint8_t);
+
+  void addHandler(Level level, Print &);
+  void removeHandler(Print &);
+
+ private:
+  struct LogHandler {
+    const uint8_t mask;
+    Print &stream;
+
+    static uint8_t makeMask(Level level);
+    LogHandler(Level level, Print &stream)
+        : mask(makeMask(level)), stream(stream) {}
+  };
+
+  std::forward_list<LogHandler> handlers;
+};
+
+extern ArduinoSimpleLogging Logger;
+
+#endif  // ARDUINOLOGGING_H
