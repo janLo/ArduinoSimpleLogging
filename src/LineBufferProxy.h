017ffd080e61bdc55f96184595a2a95808315296
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-12-14 21:18:59 +0100

    Add LineBufferProxy to the library
    This adds a LineBufferProxy. It is a simple base class to add line
    buffered logging for log targets that handle lines or messages instead
    of stream (e.g. syslog).
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

---
+++ src/LineBufferProxy.h
@@ -0,0 +1,71 @@
+/**
+ * ArduinoSimpleLogging
+ *
+ * https://github.com/janLo/ArduinoSimpleLogging
+ *
+ * Copyright (c) 2017 Jan Losinski.
+ * MIT License
+ */
+
+#ifndef LINEBUFFERPROXY_H
+#define LINEBUFFERPROXY_H
+
+#include <Print.h>
+
+template <int Buffsize>
+class LineBufferProxy : public Print {
+ public:
+  LineBufferProxy() : position(0) {}
+
+  size_t write(uint8_t byte) override {
+    buffer[position] = byte;
+    position++;
+
+    if (position >= Buffsize || byte == '\n') {
+      doFlush();
+    }
+
+    return 1;
+  }
+  size_t write(const uint8_t* incomming, size_t size) override {
+    size_t start = 0;
+
+    while (size > 0) {
+      size_t fit = std::min(size, Buffsize - position);
+
+      bool newline = false;
+      for (size_t i = 0; i < fit; ++i) {
+        buffer[position + i] = incomming[start + i];
+        if (incomming[i + start] == '\n') {
+          newline = true;
+          fit = i + 1;
+          break;
+        }
+      }
+
+      position += fit;
+
+      if (position >= Buffsize || newline) {
+        doFlush();
+      }
+
+      start += fit;
+      size -= fit;
+    }
+  }
+
+ protected:
+  virtual void flush(const char* buff) = 0;
+
+ private:
+  void doFlush() {
+    buffer[position] = '\0';
+    flush(reinterpret_cast<char*>(buffer));
+    position = 0;
+  }
+
+  uint8_t buffer[Buffsize + 1];
+  size_t position;
+};
+
+#endif  // LINEBUFFERPROXY_H
